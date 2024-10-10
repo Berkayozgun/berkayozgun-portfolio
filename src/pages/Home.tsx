af2df9fc42ca802a3bb6a79075aa6598a9aa86dd
Berkayozgun <berkayozgun001@gmail.com>
2024-10-10 19:10:55 +0300

    Refactor Home component to include Navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,12 @@ import HeroSection from "../components/HeroSection";
 import AboutSection from "../components/AboutSection";
 import ProjectsSection from "../components/ProjectsSection";
 import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 const Home: React.FC = () => {
   return (
     <div>
+      <Navbar/>
       <HeroSection />
       <AboutSection />
       <ProjectsSection />
